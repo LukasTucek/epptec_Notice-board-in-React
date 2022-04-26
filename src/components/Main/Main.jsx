f680be78e37ed99ab6ba692bef14f1a853631008
Lukas Tucek <sp7dy@seznam.cz>
2022-04-26 23:51:00 +0200

    update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,13 +4,14 @@ import './style.css';
 import { useEffect, useState } from 'react';
 
 const Main = () => {
-    const [posts, setPosts] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [comments, setComments] = useState([]);
+    const [posts, setPosts] = useState(null);
+    const [users, setUsers] = useState(null);
+    const [comments, setComments] = useState(null);
 
 
 //async function -> potřeba využít async/await? Jinak budeme muset čekat na odpovědi. Zde ale nevím, kdy se mi vrátí data.
 //"The keyword await makes JavaScript wait until that promise settles and returns its result."
+//Všechny 3 dotazy probíhají najednou
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/posts`)
             .then(response => response.json())
@@ -28,6 +29,7 @@ const Main = () => {
 
     //finding author
     const authorName = (authorId) => {
+
         return users.find((user) => {
            if (user.id === authorId) {
                 return true
@@ -45,21 +47,26 @@ const Main = () => {
 
     return (
         <>
-    
-            {posts.map(post => (
-                <div key={post.id} className="post">
-                    <h2>{post.title}</h2>
-                    <h3>Author - {authorName(post.userId)}</h3>
-                    <p>{post.body}</p>
-                    <button className="button">Show more...</button>
-                    <button className="button">Comments ({commentsCount(post.Id)})</button>
-                </div>
-            ))};
+        { 
+            (posts === null || users === null || comments === null) //když je alespoň jedno z nich null
+            ? <p class="loading">loading data...</p>
+            :   <ul>
+                {posts.map(post => (
+                    <div key={post.id} className="post">
+                        <h2>{post.title}</h2>
+                        <h3>Author - {authorName(post.userId)}</h3>
+                        <p>{post.body}</p>
+                        <button className="button">Show more...</button>
+                        <button className="button">Comments ({commentsCount(post.Id)})</button>
+                    </div>
+                ))}
+                </ul>
+        }
 
             {/*<Post title={post.title} body={post.body} author={post.author} />*/}
 
         </>
-    )
+    );
 
 };
 
